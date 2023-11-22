e4e18621248d99fbe7bbc68cd9596dbb29b9175c
unknown <jp.campos021@gmail.com>
2023-11-22 09:38:47 -0300

    FrontEnd

--- Models/Carrinho.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace UtopiaAPI.Models;
-
-public class Carrinho{
-    [Key]
-    public int? Id { get; set; }
-    public List<ItemCarrinho>? Items { get; set; }
-}
